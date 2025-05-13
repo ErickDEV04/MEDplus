cba70218f4bd814a6395aba81c427bb013e10cc6
Erick Silva <erick04develope@gmail.com>
2025-05-13 19:07:54 -0300

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,1 +1,12 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'medplus';
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
+}
+?>
